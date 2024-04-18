b1ff62228a6cf2e0745e938905225400b73a66c4
Valeria20021 <valerifuentes123@hotmail.es>
2024-04-18 17:36:46 -0500

    conexion con bd

--- app/main.py
+++ app/main.py
@@ -1,17 +1,83 @@
 import postcodes
 import read_csv
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+import os
+
+
+load_dotenv()
+
+app = FastAPI()
+####################################################################
+# Configuración de la conexión a MongoDB
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client['postcodes_db']
+collection = db['postcodes']
+
+
+
+@app.get('/add_postcodes')
+def get_list():
+    try:
+        item, extra_data, outcode = run()
+
+        # Guardar los datos en la base de datos MongoDB
+        post_data = {
+            'coordenadas': item,
+            'postcode': outcode,
+            'data': extra_data
+        }
+        collection.insert_one(post_data)
+
+        # Devolver los datos como respuesta
+        return {"message": "Fila añadida a la base de datos correctamente", "data": post_data}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {e}")
+
+#############################################
+
+
+'''
+@app.get('/postcodes')
+def get_list():
+    item, extra_data, outcode = run()
+    format = {
+        'coordenadas' : item,
+        'postcode' : outcode,
+        'data' : extra_data
+    }
+    return format
+'''
 
 def run():
     csv_path_file = './coordenates.csv'
     #leer archivo y dar formato a coordenadas
     coordinate_data = read_csv.read_csv(csv_path_file)
     formatted_coordinates = read_csv.format_coordinates_get(coordinate_data)
+
+    requests_sent = 0
+
     for item in formatted_coordinates:
-        #ejecua funcion para cada coordenada
-        outcode= postcodes.get_postcode(item)
-        print("Coordinate:", item)
-        print("Postcode:", outcode)
-   
+        try:
+            #ejecuta funcion para cada coordenada
+            outcode, extra_data= postcodes.get_postcode(item)
+            requests_sent += 1
+        except ValueError as e:
+            print(f'No se encontraron códigos postales para la coordenada: {item}: {e}')
+
+            continue
+            # print("extra_data:", extra_data)
+            # print("Coordinate:", item)
+            # print("Postcode:", outcode)
+        if requests_sent >= 4:
+            break
+    return item, extra_data, outcode
+
+
+
 
 if __name__ == '__main__':
     run()
--- app/postcodes.py
+++ app/postcodes.py
@@ -1,6 +1,5 @@
 import requests
 
-
 def get_postcode(item):
      url= 'https://api.postcodes.io/postcodes?'
      response = requests.get(url,item)
@@ -10,11 +9,12 @@ def get_postcode(item):
      result = data.get('result', [])
 
      if result is None:
-            print("No se encontraron códigos postales para la coordenada:", item)
             return ['null']
      #Almacena cada codigo
      postcodes = [codes.get('postcode', 'N/A') for codes in result]
-     return postcodes
+
+     return postcodes,data
+
 
 
 
