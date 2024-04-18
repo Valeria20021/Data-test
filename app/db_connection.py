ff9ababe92c62896a2114fbb140086e2a941ccdf
Valeria20021 <valerifuentes123@hotmail.es>
2024-04-18 01:09:19 -0500

    conexion base de datos

---
+++ app/db_connection.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+# Datos de conexión
+host = "localhost"
+user = "root"
+password = ""
+dbname = "cripto_bd"
+
+try:
+    # Conexión a la base de datos
+    conn = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=dbname
+    )
+
+    # Verifica si hay errores en la conexión
+    if conn.is_connected():
+        print("Conexión exitosa a la base de datos")
+
+except mysql.connector.Error as e:
+    print("Conexión fallida:", e)
+finally:
+    # Cerrar la conexión
+    if 'conn' in locals() and conn.is_connected():
+        conn.close()
+        print("Conexión cerrada")
