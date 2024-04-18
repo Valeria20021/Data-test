75eb934fbc04599c42a18366bb37a54125ed4e7d
Valeria20021 <valerifuentes123@hotmail.es>
2024-04-17 23:14:03 -0500

    implementación de validaciones y errores

--- app/postcodes.py
+++ app/postcodes.py
@@ -4,17 +4,17 @@ import requests
 def get_postcode(item):
      url= 'https://api.postcodes.io/postcodes?'
      response = requests.get(url,item)
-
      print(response.status_code)
 
      data = response.json()
      result = data.get('result', [])
-     postcodes_data = []
-     #Almacena cada postcode en una lista para cada coordenada
-     for codes in result:
-            postcode = codes.get('postcode', 'N/A')
-            postcodes_data.append(postcode)
 
-     return postcodes_data
+     if result is None:
+            print("No se encontraron códigos postales para la coordenada:", item)
+            return ['null']
+     #Almacena cada codigo
+     postcodes = [codes.get('postcode', 'N/A') for codes in result]
+     return postcodes
+
 
 
--- app/read_csv.py
+++ app/read_csv.py
@@ -1,36 +1,42 @@
 import pandas as pd
+import numpy as np
 import os
 
 def read_csv(path):
   if not os.path.exists(path):
     raise FileNotFoundError(f'No se encuentra el archivo {path}')
 
-  try:
-    csv_data = pd.read_csv(path, sep='|')
+  csv_data = pd.read_csv(path, sep='|').replace("nan", np.nan)
 
-    if csv_data.duplicated().any():
-            duplicate_rows = csv_data[csv_data.duplicated()].index.tolist()
-            raise ValueError(f"El archivo CSV contiene filas duplicadas en las filas: {duplicate_rows}")
+  #Eliminar datos duplicados o sin valor
+  csv_data.drop_duplicates(inplace=True)
+  csv_data.dropna(subset=['lat', 'lon'], inplace=True)
+
+  if csv_data.duplicated().any():
+     csv_data[csv_data.duplicated()].index.tolist()
+     raise ValueError(f"El archivo CSV contiene filas duplicadas")
+  
+  if csv_data['lat'].isnull().any() or csv_data['lon'].isnull().any():
+     raise ValueError("El archivo CSV contiene valores NaN en las columnas 'lat' o 'lon'.")
+
 
     #formato key-value que retorna diccionarios
-    data = csv_data.to_dict('records')
-    return data
-  
-  except Exception as error:
-     raise RuntimeError(f'No se puede leer el archivo CSV {error}')
+  data = csv_data.to_dict('records')
+  return data
 
 
 def format_coordinates_get(data):
     
      #Formato para GET
      formatted_coordinates = []
-
-     if data['lat'].isnull().any() or data['lon'].isnull().any():
-        data_null_coordinates= print("El DataFrame contiene valores faltantes en las columnas 'lat' o 'lon'.")
-
+     
      for item in data:
          lat = str(item['lat']).replace("'", "").replace(",",".")
          lon = str(item['lon']).replace("'", "").replace(",",".")
-         formatted_coordinates.append(f"lon={lon}&lat={lat}")
+
+         # Verificar si las coordenadas no son 'nan' antes de agregarlas
+         if lat != 'nan' and lon != 'nan':
+          formatted_coordinates.append(f"lon={lon}&lat={lat}")
+
 
      return formatted_coordinates
