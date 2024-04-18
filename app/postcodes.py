c4c85c227069b4a08e96e94e6331cd150a0a4ae9
Valeria20021 <valerifuentes123@hotmail.es>
2024-04-18 17:23:37 -0500

    insercion a base de datos

--- app/postcodes.py
+++ app/postcodes.py
@@ -3,13 +3,14 @@ import requests
 def get_postcode(item):
      url= 'https://api.postcodes.io/postcodes?'
      response = requests.get(url,item)
+     print(response.status_code)
+
      data = response.json()
      result = data.get('result', [])
 
      if result is None:
-            print("No se encontraron códigos postales para la coordenada:", item)
             return ['null']
-     #Almacena cada codigo en la lista
+     #Almacena cada codigo
      postcodes = [codes.get('postcode', 'N/A') for codes in result]
 
      return postcodes,data
